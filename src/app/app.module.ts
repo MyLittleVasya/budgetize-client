ab865422c299fb5d9123ad9bdc6e965e22830aae
Max Shevchuk <www.max_shevchuk@knu.ua>
2023-08-07 16:11:02 +0300

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ContentComponent } from './components/content/content.component';
 import { HistoryComponent } from './components/history/history.component';
 import {BarChartModule, PieChartModule} from "@swimlane/ngx-charts";
 import {RouterModule} from "@angular/router";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {SettingsComponent} from "./components/settings/settings.component";
+import {NotificationsComponent} from "./components/notifications/notifications.component";
+import {BudgetComponent} from "./components/budget/budget.component";
 
 @NgModule({
   declarations: [
@@ -23,6 +27,10 @@ import {RouterModule} from "@angular/router";
     BarChartModule,
     RouterModule.forRoot([
       {path: '', component: ContentComponent},
+      {path: 'profile', component: ProfileComponent},
+      {path: 'settings', component: SettingsComponent},
+      {path: 'notifications', component: NotificationsComponent},
+      {path: 'budget', component: BudgetComponent},
     ]),
   ],
   providers: [],
